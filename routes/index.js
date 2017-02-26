40d56e785db442d364139d93ca2acb6eada3086f
Eben Bosman <eben.bosman@gmail.com>
2017-02-26 14:32:15 +0200

    aggregating, but incorrectly

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ var express = require('express'),
     fs = require('fs'),
     path = require('path'),
     pathToUpload = path.join(__dirname, '../public/upload'),
-    csv = require("fast-csv");
+    csv = require("fast-csv"),
+    enumerable = require('linq');
+
 router.get('/', function (req, res) {
     res.render('index', {exercise: 'CSV Upload'});
 });
@@ -49,24 +51,80 @@ function processCsvLoansFile(file) {
             RawLoans.push(data);
         })
         .on("end", function () {
-            aggregateData(RawLoans);
+            var x = aggregateData(RawLoans);
+
+            var xx = "";
         });
 
     stream.pipe(csvStream);
 }
 
 function aggregateData(rawLoans) {
+    var loansObject = convertToJson(rawLoans);
 
-    // var headerItems = rawLoans[0];
-    //
-    // headerItems.forEach(function (headerItem, headerItemIndex) {
-    // });
-    //
-    // rawLoans.forEach(function (item, index) {
-    //
-    // });
-    //
-    // writeAggregatedData(headerItems, "");
+    var simplifiedDate = enumerable
+        .from(loansObject)
+        .select(function (x) {
+            var date = x['Date'].substring(3);
+            date = date.substring(0, 3);
+
+            return {
+                'MSISDN': x['MSISDN'],
+                'Network': x['Network'],
+                'Product': x['Product'],
+                'Date': date,
+                'Amount': x['Amount']
+            };
+        })
+        .distinct()
+        .toArray();
+
+    var tested = [];
+    var aggregate = [];
+
+    simplifiedDate.forEach(function (itemTest, indexTest) {
+        var test = itemTest.MSISDN + " " + itemTest.Network + " " + itemTest.Product + " " + itemTest.Date;
+        if (tested.indexOf(test) === -1) {
+            tested.push(test);
+
+            var isdn = itemTest.MSISDN,
+                network = itemTest.Network,
+                product = itemTest.Product,
+                date = itemTest.Date,
+                sum = 0.00;
+
+            simplifiedDate.forEach(function (item, index) {
+                if (item.MSISDN === isdn && item.Network === network && item.Product === product && item.Date === date) {
+                    sum += parseFloat(item.Amount);
+                }
+            });
+
+            aggregate.push({MSISDN: isdn, Network: network, Products: product, Date: date, Amount: sum});
+        }
+    });
+
+    return aggregate;
+}
+
+function convertToJson(rawLoans) {
+    var jsonLoans = "[";
+
+    for (var j = 1; j < rawLoans.length - 1; j++) {
+        var row = "{";
+        for (var i = 0; i < rawLoans[0].length; i++) {
+            row = row + "\"" + rawLoans[0][i] + "\": \"" + rawLoans[j][i].replace("'", "") + "\","
+        }
+        row = replaceLastInstanceOfChar(row);
+        jsonLoans = jsonLoans + row + "},";
+    }
+
+    jsonLoans = replaceLastInstanceOfChar(jsonLoans) + "]";
+
+    return JSON.parse(jsonLoans);
+}
+
+function replaceLastInstanceOfChar(jsonString) {
+    return jsonString.replace(/,\s*$/, "");
 }
 
 function writeAggregatedData(headerItems, data) {
